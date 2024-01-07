1c789b58037bd9309963fd80a795be09c64d7b97
pehlo2 <132269171+pehlo2@users.noreply.github.com>
2024-01-07 21:02:48 +0000

    show cubes on home page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
+const cubeManager = require('../managers/cubeManager.js')
 
 //routes
 router.get('/', (req,res)=>{
+    const cubes = cubeManager.getAllCubes();
 
-    res.render('index')
+    res.render('index',{cubes})
 })
 router.get('/about', (req,res)=>{
 
