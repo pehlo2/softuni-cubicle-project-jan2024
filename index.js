fa44decd05fe286ac0a393397704690e2fd2fa90
pehlo2 <132269171+pehlo2@users.noreply.github.com>
2024-01-07 16:41:33 +0000

    setup router

--- index.js
+++ index.js
@@ -3,20 +3,12 @@ let app  = express()
 const PORT = 5000
 const expressConfig = require('./config/expressConfig.js')
 const handlebarsConfig = require('./config/handlebarConfig.js')
+const homeControler = require('./controllers/homeController.js')
 
 expressConfig(app)
 handlebarsConfig(app)
+app.use(homeControler)
 
 
-//add HandleBars 
-
-//setup ssc and static files
-app.use(express.static('static'))
-
-//routes
-app.get('/', (req,res)=>{
-
-    res.render('index')
-})
 
 app.listen(PORT ,()=>{ console.log(`Server is runing port ${PORT}`);})
