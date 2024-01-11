0c8494557bd359fc77276752f4cad4c1eed1741d
pehlo2 <132269171+pehlo2@users.noreply.github.com>
2024-01-11 21:29:26 +0000

    setup mongoose

---
+++ config/dB.js
@@ -0,0 +1,10 @@
+let mongoose = require('mongoose')
+
+const uri = "mongodb://127.0.0.1:27017/cubicle"
+
+async function dbConnect(){
+await mongoose.connect(uri)
+
+}
+
+module.exports = dbConnect
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const dbConnect = require('./config/dB.js')
 let app  = express()
 const PORT = 5000
 const expressConfig = require('./config/expressConfig.js')
@@ -8,8 +9,9 @@ const routes = require('./routes.js')
 
 expressConfig(app)
 handlebarsConfig(app)
+dbConnect().then(()=>console.log("Db connected succssesfully")).catch(err=>console.log('Db error :',err))
+
+
+
 app.use(routes)
-
-
-
 app.listen(PORT ,()=>{ console.log(`Server is runing port ${PORT}`);})
