805d28bbf5c22dc3e87ec3a11e6934e19c4f2c0f
pehlo2 <132269171+pehlo2@users.noreply.github.com>
2024-01-20 17:46:53 +0000

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 let mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 let userSchema = new mongoose.Schema({
     username: String,
@@ -11,6 +12,11 @@ userSchema.virtual('repeatPassword').set(function (value) {
         throw new mongoose.MongooseError('Passwords dont Match')
     }
 
+})
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10)
+
+ this.password =hash
 })
 
 let User = mongoose.model('User', userSchema)
